bc0600df4f46b0e19cab10bd0c49026bfd6415c6
Mario Mori <mario_mori@hotmail.com>
2022-10-02 03:15:53 -0300

    New exercise

---
+++ src/main/kotlin/SmallestMultiple.kt
@@ -0,0 +1,26 @@
+/*
+You need to find the smallest positive number that is evenly divisible by all of the numbers from 1 to N
+
+sample input
+2
+3
+10
+
+sample output
+6
+2520
+ */
+
+fun main() {
+    val t = readLine()!!.trim().toInt()
+
+    for (test in 1..t) {
+        val n = readLine()!!.trim().toInt()
+
+        // Create a list of numbers that goes from 1 to N
+        val numbersToDivide = listOf((1..n))
+
+
+
+    }
+}
